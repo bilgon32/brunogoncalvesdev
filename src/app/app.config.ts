39f27cd80aa7b630d0d53d6d2a06331fbb603c17
Bruno Daniel Gonçalves <34423090+bilgon32@users.noreply.github.com>
2025-09-04 16:32:11 -0300

    Convert project to CloudFlare Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,12 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient(withFetch()), 
+  providers: [
+    provideHttpClient(withFetch()), 
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes)
   ]
 };
